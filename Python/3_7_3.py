52cbebdcde14968872d7d41c4bc8aca4fb396527
Sergey Dolgov <SVDolgov@yandex.ru>
2017-07-06 09:45:07 +0900

    Task 3_7_3 complete.

--- Python/3_7_3.py
+++ Python/3_7_3.py
@@ -1,15 +1,23 @@
-d = int(input())
-ds = set()
-for i in range(d):
-    ds.add(input())
-l = int(input())
-ls = list()
-ew = set()
-for i in range(l):
-    s = input().split()
-    ls.append(s)
-    for w in s:
-        if w not in ds:
-            ew.add(s)
-for w in ew:
-    print(w)
+'''
+Курс Программирование на Python — 3 Функции. Словари. Интерпретатор. Файлы. Модули.
+
+Через стандартный ввод подаётся следующая структура: первой строкой — количество d записей в списке известных слов, после передаётся d строк с одним словарным словом на строку, затем — количество l строк текста, после чего — l строк текста.
+
+Программа выводит слова из текста, которые не встречаются в словаре. Регистр слов не учитывается. Порядок вывода слов произвольный. Слова, не встречающиеся в словаре, не должны повторяться в выводе программы
+'''
+wcount = int(input())
+wset = set()
+for i in range(wcount):
+    wset.add(input().lower())
+lcount = int(input())
+llist = list()
+ewords = set()
+for i in range(lcount):
+    llist = input().split()
+    for word in llist:
+        if word.lower() not in wset:
+            ewords.add(word)
+for word in ewords:
+    print(word)
+
+# EOF
